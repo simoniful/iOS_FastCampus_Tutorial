7238f31b0f057b2e4cf960d5ed8c0ad648db669a
sanghunlee <simoniful0@gmail.com>
2022-06-07 00:30:11 +0900

    Add: 다음 카페/블로그 검색 튜토리얼 (3)
    Codebased UI 구성
    bind()를 통한 UI 연결 및 delegate 래핑
    리액티브한 Alert 구성과 관련하여 베이스 작성
    추가적인 코드 정돈 및 bind() 관련 정리 필요

---
+++ 4. Upper Intermediate/BlogCafeSearch/BlogCafeSearch/Presentation/MainViewController/SubComponents/SearchBar/SearchBar.swift
@@ -0,0 +1,83 @@
+//
+//  SearchBar.swift
+//  BlogCafeSearch
+//
+//  Created by Sang hun Lee on 2022/06/06.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+class SearchBar: UISearchBar {
+    let disposeBag = DisposeBag()
+    let searchButton = UIButton()
+    let searchButtonTapped = PublishRelay<Void>()
+    var shouldLoadResult = Observable<String>.of("")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        bind()
+        attribute()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func bind() {
+        // 서치바의 서치버튼이 탭 되었을 경우
+        // 커스텀 버튼이 탭 되었을 경우
+        Observable<Void>
+            .merge(
+                self.rx.searchButtonClicked.asObservable(),
+                searchButton.rx.tap.asObservable()
+            )
+            .bind(to: searchButtonTapped)
+            .disposed(by: disposeBag)
+        
+        // 키보드 내림
+        searchButtonTapped
+            .asSignal()
+            .emit(to: self.rx.endEditing)
+            .disposed(by: disposeBag)
+        
+        self.shouldLoadResult = searchButtonTapped
+            .withLatestFrom(self.rx.text) { $1 ?? "" }
+            .filter{ !$0.isEmpty }
+            .distinctUntilChanged()
+        
+        
+    }
+    
+    func attribute() {
+        searchButton.setTitle("검색", for: .normal)
+        searchButton.setTitleColor(.systemBlue, for: .normal)
+        
+    }
+    
+    func layout() {
+        addSubview(searchButton)
+        
+        searchTextField.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(12)
+            $0.trailing.equalTo(searchButton.snp.leading).offset(-12)
+            $0.centerY.equalToSuperview()
+        }
+        
+        searchButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(12)
+        }
+    }
+}
+
+extension Reactive where Base: SearchBar {
+    var endEditing: Binder<Void> {
+        return Binder(base) { base, _ in
+            base.endEditing(true)
+        }
+    }
+}
