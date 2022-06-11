5fb99b163af4fc055e9650625f0688f95d96b337
sanghunlee <simoniful0@gmail.com>
2022-06-11 22:52:49 +0900

    Add: 편의점 검색 튜토리얼 (1)
    KaKao Map SDK 세팅 - 프레임 워크 매핑, 브릿지 헤더 구성
    API, APP Key 세팅

---
+++ 5. Advanced/ConvenienceStore/ConvenienceStore/BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgingHeader.h
+//  ConvenienceStore
+//
+//  Created by Sang hun Lee on 2022/06/11.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import <DaumMap/MTMapView.h>
+
+
+#endif /* BridgingHeader_h */
